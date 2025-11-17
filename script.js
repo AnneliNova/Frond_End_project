8f5940d61e56b0aaed1e7a53c18e8cbd72702307
icsng <csengeigyarto@gmail.com>
2025-11-17 21:11:24 +0100

    js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// се потом //
+const toggle = document.getElementById('myToggle');
+
+toggle.addEventListener('change', () => {
+    if (toggle.checked) {
+        window.location.href = 'theme.html';
+    } else {
+        window.location.href = 'index.html';
+    }
+});
+
+if (window.location.href.includes('theme.html')) {
+    toggle.checked = true;
+}
