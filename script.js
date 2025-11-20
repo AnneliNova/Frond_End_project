dcb592068d62bca53c9cc9372473a1c84029cb97
icsng <csengeigyarto@gmail.com>
2025-11-20 17:48:36 +0100

    fixed bugs

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const toggle = document.getElementById('myToggle');
+// const toggle = document.getElementById('myToggle');
 
-toggle.addEventListener('change', () => {
-    if (toggle.checked) {
-        window.location.href = 'theme.html';
-    } else {
-        window.location.href = 'index.html';
-    }
-});
+// toggle.addEventListener('change', () => {
+//     if (toggle.checked) {
+//         window.location.href = 'theme.html';
+//     } else {
+//         window.location.href = 'index.html';
+//     }
+// });
 
-if (window.location.href.includes('theme.html')) {
-    toggle.checked = true;
-}
+// if (window.location.href.includes('theme.html')) {
+//     toggle.checked = true;
+// }
 
 function setCookie(name, value, days = 30) {
     const date = new Date();
