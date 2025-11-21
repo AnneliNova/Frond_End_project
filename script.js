d68a9befcb55ec43d550481841bdbcba37bed323
icsng <csengeigyarto@gmail.com>
2025-11-21 15:42:18 +0100

    fixed contacts

--- script.js
+++ script.js
@@ -178,11 +178,12 @@ renderSchedule();
 function closeModal() {
 let modal = document.getElementById('contactModal');
 let overlay = document.getElementById('modalOverlay'); 
-
+ 
 if (modal) {
     modal.style.display = 'none';
 }
 if (overlay) {
     overlay.style.display = 'none';
 }
+window.location.href = 'index.html';
 }
