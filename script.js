c4e9000874ffc40a2ef924a9eb3415fcac3cb4c1
icsng <csengeigyarto@gmail.com>
2025-11-21 15:18:03 +0100

    add contacts

--- script.js
+++ script.js
@@ -174,3 +174,15 @@ function renderSchedule() {
 }
 
 renderSchedule();
+
+function closeModal() {
+let modal = document.getElementById('contactModal');
+let overlay = document.getElementById('modalOverlay'); 
+
+if (modal) {
+    modal.style.display = 'none';
+}
+if (overlay) {
+    overlay.style.display = 'none';
+}
+}
